2e1085d4f2db766f22ba18e19c13b6eb44e0e9f8
MeepyMay <meepymay@gmail.com>
2018-08-23 15:52:00 -0400

    fixed the connection

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,15 @@ var connection;
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
+    host: "ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    port: process.env.PORT || 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });  
 }
-connection = mysql.createConnection({
-  host: "ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
-  port: process.env.PORT || 8889,
-  user: "b0p22du7thy8w0ql",
-  password: "pll3w8rst3e0g9y2",
-  database: "s0ua41ek6imoht3y"
-});
 
 connection.connect(function(err) {
   if (err) {
